042c575d27feecfbd78310366fb87863fb500a98
TaewanKIM20 <fnzk2019@g.skku.edu>
2024-08-14 19:11:44 +0900

    minor fix

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,11 +9,11 @@ const about = {
   name: 'Taewan Kim',
   role: 'Back End Engineer',
   description: `
-  I am Taewan Kim, a budding back-end developer who enrolled at Sungkyunkwan University in 2019 with a focus on back-end development. Here are some highlights of my journey so far:
-  - Currently involved as a member in two startups, excluding various side projects.
-  - Primarily working with Node.js as part of my ongoing study of back-end development stacks.
-  - Spent a month in Thailand in 2023 on an ICT volunteering project.
-  - Actively participating in Voluntain, where I help operate an online educational platform for coding as part of international volunteer service.
+  I am Taewan Kim, a budding back-end developer who enrolled at Sungkyunkwan University in 2019 with a focus on back-end development. Here are some highlights of my journey so far:\n
+  - Currently involved as a member in two startups, excluding various side projects.\n
+  - Primarily working with Node.js as part of my ongoing study of back-end development stacks.\n
+  - Spent a month in Thailand in 2023 on an ICT volunteering project.\n
+  - Actively participating in Voluntain, where I help operate an online educational platform for coding as part of international volunteer service.\n
 `,
   resume: 'https://example.com',
   social: {
@@ -28,9 +28,9 @@ const projects = [
   {
     name: 'Meeting Go',
     description: `
-  'Meeting Go' is a social discovery app for Korean college students. As a lead back-end developer, I focused on:
-  - Developing APIs and real-time chats using MySQL and Socket.IO.
-  - Ensuring application integrity through code reviews and testing.
+  'Meeting Go' is a social discovery app for Korean college students. As a lead back-end developer, I focused on:\n
+  - Developing and deploying APIs and real-time chats using MySQL and Socket.IO.\n
+  - Ensuring application integrity through code reviews and testing.\n
 `,
     stack: ['Node.js', 'Mysql', 'Flutter'],
     sourceCode: 'https://github.com',
@@ -39,8 +39,8 @@ const projects = [
   {
     name: 'Splash',
     description:
-      `'Splash' is a marine leisure sports booking platform. As the founder and lead back-end developer, I was responsible for:
-  - Designing and deploying all back-end APIs.`,
+      `'Splash' is a marine leisure sports booking platform. As the founder and lead back-end developer, I was responsible for:\n
+  - Designing and deploying all back-end APIs.\n`,
     stack: ['Node.js', 'Mysql', 'React'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
@@ -48,10 +48,10 @@ const projects = [
   {
     name: 'Voluntain',
     description: `
-  'Voluntain' is an educational video platform for international students. I have enhanced and currently manage the project by:
-  - Refactoring the existing codebase.
-  - Adding user APIs.
-  - Redesigning and deploying updates.
+  'Voluntain' is an educational video platform for international students. I have enhanced and currently manage the project by:\n
+  - Refactoring the existing codebase.\n
+  - Adding user APIs.\n
+  - Redesigning and deploying updates.\n
 `
 ,
     stack: ['Strapi', 'Next.js', 'React'],
