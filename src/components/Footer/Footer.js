0dcab0b8e27278faeb308f92d680ef5f344bb2b5
TaewanKIM20 <fnzk2019@g.skku.edu>
2024-08-14 19:06:17 +0900

    minor ifx

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,12 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-
+    <a
+      href='https://github.com/TaewanKIM20'
+      className='link footer__link'
+    >
+      Go to Github/TaewanKIM20
+    </a>
   </footer>
 )
 
