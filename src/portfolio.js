81afd6726953cc5a0f24f59b69bd9cca22da23c5
TaewanKIM20 <fnzk2019@g.skku.edu>
2024-08-16 12:09:37 +0900

    Add skills

--- src/portfolio.js
+++ src/portfolio.js
@@ -60,6 +60,9 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
+  'C',
+  'C++',
+  'python',
   'Node.js',
   'Strapi',
   'MySQL',
